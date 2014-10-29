5286b09cc280b4f85edffacb973e7eadd8986f60
ntoro <ntoro@84e480dd-4284-0410-bcb6-dc83b1e7cf5b>
2014-10-29 15:06:56 +0000

    Move archive-utils and archive-data-provider-api to pride-archive

---
+++ src/main/java/uk/ac/ebi/pride/prider/dataprovider/project/ProjectTagProvider.java
@@ -0,0 +1,13 @@
+package uk.ac.ebi.pride.prider.dataprovider.project;
+
+import uk.ac.ebi.pride.prider.dataprovider.entity.EntityProvider;
+
+/**
+ * @author Jose A. Dianes
+ * @version $Id$
+ */
+public interface ProjectTagProvider extends EntityProvider {
+
+    public String getTag();
+
+}
